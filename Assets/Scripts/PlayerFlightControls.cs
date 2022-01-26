816b620505df52c5cc7a7d1b443315ef361a1b67
Aidan McChristie <Aidan.mcchristie@gmail.com>
2022-01-25 18:29:34 -0600

    adds project beginnings

---
+++ Assets/Scripts/PlayerFlightControls.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerFlightControls : MonoBehaviour
+{
+    public float forwardSpeed = 25f, strafeSpeed = 7.5f, hoverSpeed = 5f;
+    private float activeForwardSpeed, activeStrafeSpeed, activeHoverSpeed;
+    private float forwardAcceleration = 2.5f, strafeAcceleration = 2f, hoverAcceleration = 2f;
+
+
+    public float verticleLookSpeed = 7f, horizontalLookSpeed = 10f, rollSpeed = 10f;
+    private float activeRollSpeed;
+
+    private Vector3 lookRotation;
+
+    private Vector2 movementInput;
+
+    private float strafeInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleRotation();
+        HandleMovement();
+        /*
+         * use this for total control over speed
+         */
+        //transform.position += this.transform.forward * activeForwardSpeed * Time.deltaTime;
+        /*
+         * use this for auto move forward
+         */
+        transform.position += this.transform.forward * forwardSpeed * Time.deltaTime;
+        transform.position += (transform.right * activeStrafeSpeed * Time.deltaTime) + (transform.up * activeHoverSpeed * Time.deltaTime);
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+
+    void HandleMovement()
+    {
+        activeForwardSpeed = Mathf.Lerp(activeForwardSpeed, movementInput.y * forwardSpeed, forwardAcceleration * Time.deltaTime);
+        /*
+         * for strafing movement
+         */
+        activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed, strafeInput * strafeSpeed, strafeSpeed * Time.deltaTime);
+    }
+    void HandleRotation()
+    {
+        activeRollSpeed = Mathf.Lerp(activeRollSpeed, movementInput.x * rollSpeed, rollSpeed * Time.deltaTime);
+        lookRotation = new Vector3(
+            lookRotation.x * verticleLookSpeed,
+            lookRotation.y * horizontalLookSpeed,
+            lookRotation.z * rollSpeed) * Time.deltaTime;
+        lookRotation.z = -activeRollSpeed;
+
+        this.transform.Rotate(lookRotation, Space.Self);
+
+    }
+    public void OnMove(InputAction.CallbackContext callbackContext)
+    {
+        //Debug.Log("Moving");
+        movementInput = callbackContext.ReadValue<Vector2>();
+        lookRotation.z = -callbackContext.ReadValue<Vector2>().x;
+    }
+
+    public void OnLook(InputAction.CallbackContext callbackContext)
+    {
+        //Debug.Log("looking");
+        lookRotation.x = -(callbackContext.ReadValue<Vector2>().y);
+        lookRotation.y = callbackContext.ReadValue<Vector2>().x;
+    }
+
+    public void OnStrafe(InputAction.CallbackContext callbackContext)
+    {
+        strafeInput = callbackContext.ReadValue<float>();
+    }
+}
