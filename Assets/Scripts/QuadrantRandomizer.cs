0bcfaaf26175c570371f58f48ae4765e9dcdc096
Sebp23 <57201896+Sebp23@users.noreply.github.com>
2022-03-11 17:25:14 -0600

    Added comments to QuadrantRandomizer

--- Assets/Scripts/QuadrantRandomizer.cs
+++ Assets/Scripts/QuadrantRandomizer.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 
 public class QuadrantRandomizer : MonoBehaviour
 {
-    [SerializeField]
-    List<GameObject> QuadrantList;
+    private List<GameObject> QuadrantList;
     // Start is called before the first frame update
     void Start()
     {
+        //instantiate the list
         QuadrantList = new List<GameObject>();
 
+        //find all the quadrant prefabs in the 'Assets/Resources/Prefabs/Quadrants' folder and add them to the list
         foreach(var quadrant in Resources.LoadAll<GameObject>("Prefabs/Quadrants"))
         {
             QuadrantList.Add(quadrant); 
@@ -19,6 +20,7 @@ public class QuadrantRandomizer : MonoBehaviour
 
         Debug.Log(QuadrantList.Count);
 
+        //randomly generate the position of the quadrant
         GetComponent<MeshRenderer>().enabled = false;
         Instantiate(QuadrantList[Random.Range(0, QuadrantList.Count)], this.transform, false);
         Debug.Log("Generated");
