8d1d21e860eb1be595fcb548b8bb152ba4727e12
Sebp23 <57201896+Sebp23@users.noreply.github.com>
2022-03-11 17:11:55 -0600

    Updated the Random Quadrant generator to work with a list

--- Assets/Scripts/QuadrantRandomizer.cs
+++ Assets/Scripts/QuadrantRandomizer.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class QuadrantRandomizer : MonoBehaviour
 {
     [SerializeField]
-    GameObject[] Quadrants;
+    List<GameObject> QuadrantList;
     // Start is called before the first frame update
     void Start()
     {
+        QuadrantList = new List<GameObject>();
+
+        foreach(var quadrant in Resources.LoadAll<GameObject>("Prefabs/Quadrants"))
+        {
+            QuadrantList.Add(quadrant); 
+        }
+
+        Debug.Log(QuadrantList.Count);
+
         GetComponent<MeshRenderer>().enabled = false;
-        Instantiate(Quadrants[Random.Range(0, Quadrants.Length)], this.transform, false);
+        Instantiate(QuadrantList[Random.Range(0, QuadrantList.Count)], this.transform, false);
         Debug.Log("Generated");
     }
 
