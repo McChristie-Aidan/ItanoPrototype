8bebfa407a46c27a59412b04bc9e9fabd67190df
JustinH-94 <61295115+JustinH-94@users.noreply.github.com>
2022-03-18 00:51:22 -0500

    Updated Missiles to Avoid Obstacles

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -16,13 +16,19 @@ public class Missile : MonoBehaviour
 
     [SerializeField]
     float maxForce;
+    float angleModifier = .2f;
 
     Vector3 direction;
-
+    Vector3 avoidDir;
+    Vector3 obstaclePos;
+    Ray ray;
+    RaycastHit hit;
+    public LayerMask mask;
     public GameObject target;
     PlayerFlightControls pfc;
 
     Rigidbody rb;
+    bool changeDir;
 
     // Start is called before the first frame update
     void Start()
@@ -46,6 +52,20 @@ public class Missile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        DetectObstacle();
+        if (changeDir)
+        {
+            if (obstaclePos != null)
+            {
+                var heading = obstaclePos - this.transform.position;
+                float dot = Vector3.Dot(heading, this.transform.forward);
+                if (dot <= 0)
+                    changeDir = false;
+                return;
+            }
+            return;
+        }
+        Debug.Log("H");
         direction = target.transform.position - this.transform.position;
 
         this.transform.rotation = Quaternion.Slerp(
@@ -64,7 +84,6 @@ public class Missile : MonoBehaviour
 
         //rb.AddForce(this.transform.forward * speed);
         //rb.AddForce(Separation());
-
     }
 
     private void LateUpdate()
@@ -110,5 +129,75 @@ public class Missile : MonoBehaviour
     {
         MissileManager.Instance.RemoveMissile(this);
         Destroy(this.gameObject);
+    }
+
+    
+
+    void DetectObstacle()
+    {
+        if(Physics.Raycast(this.transform.position, transform.forward, out hit, 50f, mask))
+        {
+            RayCast();
+            obstaclePos = hit.transform.position;
+            changeDir = true;
+            return;
+        }
+    }
+
+    void AvoidManeuver()
+    {
+
+    }
+
+    bool FindDirectionOfNoObstacle(Vector3 dir, Ray ray)
+    {
+        Debug.DrawLine(ray.origin, ray.origin + ray.direction * 50f, Color.red);
+        if (!Physics.Raycast(ray, 50f))
+        {
+            rb.velocity = dir * speed;
+            changeDir = false;
+            return true;
+        }
+        return false;
+    }
+
+    void RayCast()
+    {
+        for (int i = 1; i < 20; i++)
+        {
+            avoidDir = (transform.forward + new Vector3(angleModifier * i, 0, 0)).normalized;
+            if (FindDirectionOfNoObstacle(avoidDir, new Ray(transform.position, avoidDir)))
+                return;
+
+            avoidDir = (transform.forward - new Vector3(angleModifier * i, 0, 0)).normalized;
+            if (FindDirectionOfNoObstacle(avoidDir, new Ray(transform.position, avoidDir)))
+                return;
+
+            avoidDir = (transform.forward - new Vector3(0, angleModifier * i, 0)).normalized;
+            if (FindDirectionOfNoObstacle(avoidDir, new Ray(transform.position, avoidDir)))
+                return;
+
+            avoidDir = (transform.forward + new Vector3(0, angleModifier * i, 0)).normalized;
+            if (FindDirectionOfNoObstacle(avoidDir, new Ray(transform.position, avoidDir)))
+                return;
+
+
+            avoidDir = (transform.forward + new Vector3(angleModifier * i, angleModifier * i, 0)).normalized;
+            if (FindDirectionOfNoObstacle(avoidDir, new Ray(transform.position, avoidDir)))
+                return;
+
+            avoidDir = (transform.forward - new Vector3(angleModifier * i, angleModifier * i, 0)).normalized;
+            if (FindDirectionOfNoObstacle(avoidDir, new Ray(transform.position, avoidDir)))
+                return;
+
+            avoidDir = (transform.forward + new Vector3(-angleModifier * i, angleModifier * i, 0)).normalized;
+            if (FindDirectionOfNoObstacle(avoidDir, new Ray(transform.position, avoidDir)))
+                return;
+
+            avoidDir = (transform.forward - new Vector3(-angleModifier * i, angleModifier * i, 0)).normalized;
+            if (FindDirectionOfNoObstacle(avoidDir, new Ray(transform.position, avoidDir)))
+                return;
+        }
+
     }
 }
